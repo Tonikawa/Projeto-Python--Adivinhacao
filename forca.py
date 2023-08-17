2ac7a8d6dbd2662feb92a84f896a261aaa86db28
Fabio Tanikawa <vinifaro.fabio@gmail.com>
2023-08-17 17:32:02 -0300

    Atualizando forca com "chute"

--- forca.py
+++ forca.py
@@ -1,12 +1,22 @@
 def jogar():
     print("Bem Vindo ao jogo da forca !!")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "python"
 
     enforcou = False
     acertou = False
 
     while(not enforcou and not acertou):
+       
+       chute = input("Qual a letra? ")
+       
+       for letra in palavra_secreta:
+          if(chute == letra):
+            print("Você acertou a letra")
+            print(chute)
+          else:
+             print("Você errou a letra")
+             
        print("jogando...")
        
 
