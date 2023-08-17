2199da8a8a1412e43d3c45438c0790ce42742f0c
Fabio Tanikawa <vinifaro.fabio@gmail.com>
2023-08-17 17:05:27 -0300

    Modificando a forca.py

--- forca.py
+++ forca.py
@@ -1,7 +1,15 @@
 def jogar():
     print("Bem Vindo ao jogo da forca !!")
-  
-  
+
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+       print("jogando...")
+       
+
     print("Fim do jogo!!")
   
 if(__name__ == "__main__"):
