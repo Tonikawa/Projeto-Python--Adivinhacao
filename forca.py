b02a23a1df9698e36d56494ff58ff83d47957ec7
Fabio Tanikawa <vinifaro.fabio@gmail.com>
2023-08-24 17:15:23 -0300

    Adicionando lista na forca

--- forca.py
+++ forca.py
@@ -2,6 +2,8 @@ def jogar():
     print("Bem Vindo ao jogo da forca !!")
 
     palavra_secreta = "banana"
+    letras_acertadas = ["_","_","_","_","_","_"]
+    print(letras_acertadas)
 
     enforcou = False
     acertou = False
@@ -15,12 +17,13 @@ def jogar():
 
        for letra in palavra_secreta:
           if(chute.upper() == letra.upper()): #tratamento de string- igualando o chute com a palavra secreta
-            print("Você acertou a letra {} na posição {}".format(chute, index))
+             letras_acertadas[index] = letra   #adicionando a letra na lista de letras acertadas na posição certa         
           index = index + 1
           #else:
              #print("Você errou a letra")
 
-       print("jogando...")
+       print(letras_acertadas)
+     
        
 
     print("Fim do jogo!!")
