a3dd30243d7a970bb1fc4c37ebb8a16dff93131e
Fabio Tanikawa <vinifaro.fabio@gmail.com>
2023-08-24 17:29:25 -0300

    loop: usuario digitou entrada errada (1 ou 2)

--- main.py
+++ main.py
@@ -4,12 +4,18 @@ print("Escolha seu jogo!")
 
 print("(1) Forca (2)Adivinhação")
 
-jogo = int(input("Qual o jogo?"))
+#jogo = int(input("Qual o jogo?"))
 
-if(jogo == 1):
-  print("Você escolheu o jogo da Forca")
-  forca.jogar()
-elif(jogo == 2):
-  print("Você escolheu o jogo de Adivinhação")
-  adivinhação.jogar()
-  
+while True:
+  jogo = int(input("Qual o jogo?"))
+  if(jogo == 1):
+    print("Você escolheu o jogo da Forca")
+    forca.jogar()
+    break
+  elif(jogo == 2):
+    print("Você escolheu o jogo de Adivinhação")
+    adivinhação.jogar()
+    break
+  else:
+    print("Pare de zoar, proxima vez escolha um número certo!")
+    
